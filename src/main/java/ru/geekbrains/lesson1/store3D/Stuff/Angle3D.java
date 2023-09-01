372b56325be84572fbfa4a260cc27c4c3cebea60
ShuMAhd <shuman@zykaika.ru>
2023-09-01 21:46:25 +0800

    first commit

---
+++ src/main/java/ru/geekbrains/lesson1/store3D/Stuff/Angle3D.java
@@ -0,0 +1,5 @@
+package ru.geekbrains.lesson1.store3D.Stuff;
+
+public class Angle3D {
+
+}
---
+++ src/main/java/ru/geekbrains/lesson1/store3D/models/Poligon.java
@@ -0,0 +1,16 @@
+package ru.geekbrains.lesson1.store3D.models;
+
+import java.util.List;
+import ru.geekbrains.lesson1.store3D.Stuff.Point3D;
+
+public class Poligon {
+
+    private List<Point3D> points;
+
+    public Poligon(List<Point3D> points) {
+        this.points = points;
+    }
+
+    public Poligon() {
+    }
+}
